3a2106733b0a3653ebdde7fa635e3f93200d1730
Pedro Casseb <casseb.phcc@gmail.com>
2026-07-15 13:52:28 -0300

    feat(account): consume transaction completed and fraud detected events

--- account-service/src/main/java/com/banking/accountservice/service/AccountEventConsumer.java
+++ account-service/src/main/java/com/banking/accountservice/service/AccountEventConsumer.java
@@ -1,4 +1,48 @@
 package com.banking.accountservice.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class AccountEventConsumer {
+
+    private final AccountService accountService;
+
+    @KafkaListener(topics = "transaction.completed")
+    public void consumeTransactionCompleted(
+            @Payload Map<String, Object> payload
+    ) {
+        try {
+            String receiverAccount = (String) payload.get("receiverAccountNumber");
+            BigDecimal amount = new BigDecimal(payload.get("amount").toString());
+
+            log.info("Crediting account: {} amount: {}", receiverAccount, amount);
+            accountService.creditBalance(receiverAccount, amount);
+        } catch (Exception e) {
+            log.error("Error crediting account: {}", e.getMessage());
+        }
+
+    }
+
+    @KafkaListener(topics = "fraud.detected")
+    public void consumeFraudDetected(
+            @Payload Map<String, Object> payload
+    ) {
+        try {
+            String accountNumber = (String) payload.get("accountNumber");
+            log.info("Fraud detected - blocking account: {}", accountNumber);
+
+            accountService.blockAccount(accountNumber);
+        } catch (Exception e) {
+            log.error("Error blocking account: {}", e.getMessage());
+        }
+    }
 }
