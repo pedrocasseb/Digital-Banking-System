9a84d209c691c8e52eb76270393fbd6a2c4bd260
Pedro Casseb <casseb.phcc@gmail.com>
2026-07-16 18:39:23 -0300

    feat(payment-service): integrate Razorpay and implement order creation

--- payment-service/src/main/java/com/banking/paymentservice/controller/PaymentController.java
+++ payment-service/src/main/java/com/banking/paymentservice/controller/PaymentController.java
@@ -1,10 +1,20 @@
 package com.banking.paymentservice.controller;
 
+import com.banking.paymentservice.dto.CreatePaymentRequest;
+import com.banking.paymentservice.dto.PaymentOrderResponse;
 import com.banking.paymentservice.service.PaymentService;
+import com.razorpay.RazorpayException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/payments")
@@ -12,5 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PaymentController {
     private final PaymentService paymentService;
-    
+
+    @PostMapping("/create-order")
+    public ResponseEntity<PaymentOrderResponse> createPaymentOrder(
+            @Valid @RequestBody CreatePaymentRequest request
+            ) throws RazorpayException {
+        return ResponseEntity.status(HttpStatus.CREATED).body(paymentService.createPaymentOrder(request));
+    }
+
+    @PostMapping("/webhook")
+    public ResponseEntity<String> handleWebhook(
+            @RequestBody Map<String, Object> payload
+            ) {
+        paymentService.handleWebhook(payload);
+        return ResponseEntity.ok("Webhook processed");
+    }
 }
--- payment-service/src/main/java/com/banking/paymentservice/service/PaymentService.java
+++ payment-service/src/main/java/com/banking/paymentservice/service/PaymentService.java
@@ -1,13 +1,77 @@
 package com.banking.paymentservice.service;
 
+import com.banking.paymentservice.dto.CreatePaymentRequest;
+import com.banking.paymentservice.dto.PaymentOrderResponse;
+import com.banking.paymentservice.entity.Payment;
+import com.banking.paymentservice.entity.PaymentStatus;
 import com.banking.paymentservice.repository.PaymentRepository;
+import com.razorpay.Order;
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class PaymentService {
     private final PaymentRepository paymentRepository;
+
+    @Value("${razorpay.key-id}")
+    private String keyId;
+
+    @Value("${razorpay.key-secret}")
+    private String keySecret;
+
+    private static final String  PAYMENT_COMPLETED_TOPIC = "payment.completed";
+    private static final String  PAYMENT_FAILED_TOPIC = "payment.failed";
+
+    public PaymentOrderResponse createPaymentOrder(
+            CreatePaymentRequest request
+    ) throws RazorpayException {
+        log.info("Creating payment order for account: {} amount: {}", request.getAccountNumber(), request.getAmount());
+
+        RazorpayClient razorpayClient = new RazorpayClient(keyId, keySecret);
+
+        int convertedAmount = request.getAmount()
+                .multiply(BigDecimal.valueOf(100))
+                .intValue();
+
+        JSONObject orderRequest = new JSONObject();
+        orderRequest.put("amount", convertedAmount);
+        orderRequest.put("currency", "USD/INR");
+        orderRequest.put("receipt", "rcpt_" + System.currentTimeMillis() + UUID.randomUUID().toString()
+                .replace("-", "").substring(0, 10)
+        );
+
+        Order razorpayOrder = razorpayClient.orders.create(orderRequest);
+
+        log.info("Razorpay order created: {}", razorpayOrder.get("id").toString());
+
+        Payment payment = Payment.builder()
+                .razorpayOrderId(razorpayOrder.get("id").toString())
+                .accountNumber(request.getAccountNumber())
+                .amount(request.getAmount())
+                .currency("USD/INR")
+                .status(PaymentStatus.CREATED)
+                .description(request.getDescription())
+                .build();
+
+        Payment savedPayment = paymentRepository.save(payment);
+
+        return new PaymentOrderResponse(
+                savedPayment.getId(),
+                razorpayOrder.get("id").toString(),
+                request.getAmount(),
+                "USD/INR",
+                "CREATED",
+                keyId
+        );
+    }
 }
